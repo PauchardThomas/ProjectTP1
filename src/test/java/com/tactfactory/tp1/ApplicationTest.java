8ed615b867acfe8c5cb95bd004c72adfa7d90c07
PauchardThomas <thomaspauch@hotmail.fr>
2018-06-06 15:09:47 +0200

    First commit

---
+++ src/test/java/com/tactfactory/tp1/ApplicationTest.java
@@ -0,0 +1,17 @@
+package com.tactfactory.tp1;
+import org.junit.Assert;
+
+import com.tactfactory.tp1.Application;
+
+public class ApplicationTest {
+
+	@org.junit.Test
+	public void testAdd() {
+		int i =2 , j=3;
+		
+		int k = Application.add(i,j);
+		
+		Assert.assertEquals(k,5);
+	}
+
+}
